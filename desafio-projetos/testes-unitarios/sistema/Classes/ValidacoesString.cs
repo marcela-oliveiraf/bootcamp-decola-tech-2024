5bcea18945ef7841680f18b76078b61dcd943a80
marcela-oliveiraf <marcelaof@dcc.ufrj.br>
2024-01-01 05:36:00 -0300

    Add conteúdo da classe

--- desafio-projetos/testes-unitarios/sistema/Classes/ValidacoesString.cs
+++ desafio-projetos/testes-unitarios/sistema/Classes/ValidacoesString.cs
@@ -5,6 +5,19 @@ using System.Threading.Tasks;
 
 namespace sistema.Classes{
     public class ValidacoesString{
-        
+        public int RetornarQuantidadeCaracteres(string texto){
+            var numeroCaracteres = texto.Length;
+            return numeroCaracteres;
+        }
+
+        public bool ContemCaractere(string texto, string textoProcurado){
+            var contem = texto.Contains(textoProcurado);
+            return contem;
+        }
+
+        public bool TextoTerminaCom(string texto, string textoProcurado){
+            var termina = texto.EndsWith(textoProcurado);
+            return termina;
+        }        
     }
 }
